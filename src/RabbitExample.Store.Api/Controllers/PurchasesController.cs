4b2041cdf1a073d5624e4de8eb8fd6cb1aa79c1a
Iury Ferreira <iury.ferreira@vaivoa.com>
2021-09-08 17:03:45 -0300

    :tada: add projects

---
+++ src/RabbitExample.Store.Api/Controllers/PurchasesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RabbitExample.Store.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PurchasesController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<PurchasesController> _logger;
+
+        public PurchasesController(ILogger<PurchasesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Store()
+        {
+            return Ok();
+        }
+    }
+}
